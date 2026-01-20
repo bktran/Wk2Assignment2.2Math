4ed4eba79ea3637b50f1cb5c7fda39817f977851
Bao Khanh Tran <ktbv.22@outlook.com>
2026-01-20 13:39:28 -0800

    Add project files.

---
+++ Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2Assignment2._2Math
+{
+    internal class Maths
+    {
+        public static int Add(int n1, int n2) 
+        {
+            return n1 + n2;
+        }
+
+        public static decimal Add(decimal n1, decimal n2, decimal n3) 
+        {
+            return n1 + n2 + n3;
+        }
+
+        public static float Multiply(float n1, float n2) 
+        { 
+            return n1 * n2; 
+        }
+
+        public static float Multiply(float n1, float n2, float n3)
+        {
+            return n1 * n2 * n3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Wk2Assignment2._2Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = Maths.Add(5,2);
+            decimal sum2 = Maths.Add(10.5m, 5.5m, 4.6m);
+            float product1 = Maths.Multiply(2f,25f);
+            float product2 = Maths.Multiply(4f,5f,6f);
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(product1);
+            Console.WriteLine(product2);
+        }
+    }
+}
